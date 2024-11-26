56c75bb5cfe95c6131605b3f9c07891e4a84ea5f
Ben Mackie <ben@mackie.studio>
2024-11-26 14:00:02 +1100

    Cleanup

--- packages/webapp/src/routes/sum/+page.server.ts
+++ packages/webapp/src/routes/sum/+page.server.ts
@@ -1,27 +1,17 @@
 import type { PageServerLoad } from './$types';
 
-// -----------------------------------------------------------------------------
-// page load
-// -----------------------------------------------------------------------------
 export const load: PageServerLoad = async (event) => {
-	// Init forms
 	const result1 = 1 + 2;
 
-    const platform = await getPlatform(event);        
-        
+    // Call sumworker via RPC.
+    const platform = getPlatform(event);
 	const result2 = await platform.env.SUM_WORKER.add(1, 2);
 
 	return { result1, result2 };
 };
 
-
-async function getPlatform(event: { platform?: App.Platform }) {
+function getPlatform(event: { platform?: App.Platform }): App.Platform {
     let { platform } = event;
-    // if (!platform && dev) {
-    //     console.warn('platform not available in dev mode -> use getPlatformProxy');
-    //     const { getPlatformProxy } = await import('wrangler');
-    //     platform = (await getPlatformProxy()) as unknown as App.Platform;
-    // }
     if (!platform) {
         throw new Error('platform not available');
     }
